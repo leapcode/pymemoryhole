f07b05c98563dc8f3ad24cd17e0c1e1f2c557be8
Kali Kaneko <kali@leap.se>
2016-04-23 23:32:15 -0400

    add setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='memoryhole',
+      version='1.0',
+      description='',
+      author='Kali Kaneko',
+      author_email='kali@leap.se',
+      url='https://leap.se',
+      packages=['memoryhole'],
+     )
