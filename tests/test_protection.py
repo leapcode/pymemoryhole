4ee90532f1690da41543f300c33a77aeeaf40a52
Kali Kaneko (leap communications) <kali@leap.se>
2016-07-07 14:29:35 +0200

    refactor tests, pytest style

--- tests/test_protection.py
+++ tests/test_protection.py
@@ -1,5 +1,4 @@
 import six
-import unittest
 from base64 import b64encode
 from email.parser import Parser
 from zope.interface import implementer
@@ -23,85 +22,85 @@ Subject: %(subject)s
     "body": BODY
 }
 
+parser = Parser()
 
-class ProtectTest(unittest.TestCase):
-    def test_pgp_encrypted_mime(self):
-        p = Parser()
-        msg = p.parsestr(EMAIL)
-        encrypter = Encrypter()
-        conf = ProtectConfig(openpgp=encrypter, obscured_headers=[])
-        encmsg = protect(msg, config=conf)
 
-        self.assertEqual(encmsg.get_payload(1).get_payload(), encrypter.encstr)
-        self._assert_body(encrypter.data, BODY+'\n')
-        self.assertEqual([TO], encrypter.encraddr)
-        self.assertEqual(encmsg.get_content_type(), "multipart/encrypted")
+def test_pgp_encrypted_mime():
+    msg = parser.parsestr(EMAIL)
+    encrypter = Encrypter()
+    conf = ProtectConfig(openpgp=encrypter, obscured_headers=[])
+    encmsg = protect(msg, config=conf)
 
-    def test_unobscured_headers(self):
-        p = Parser()
-        msg = p.parsestr(EMAIL)
-        encrypter = Encrypter()
-        conf = ProtectConfig(openpgp=encrypter, obscured_headers=[])
-        encmsg = protect(msg, config=conf)
+    assert encmsg.get_payload(1).get_payload() == encrypter.encstr
+    assert [TO] == encrypter.encraddr
+    assert encmsg.get_content_type() == "multipart/encrypted"
+    assert get_body(encrypter.data) == BODY + '\n'
 
-        self.assertEqual(encmsg['from'], FROM)
-        self.assertEqual(encmsg['to'], TO)
-        self.assertEqual(encmsg['subject'], SUBJECT)
 
-    def test_obscured_headers(self):
-        p = Parser()
-        msg = p.parsestr(EMAIL)
-        encrypter = Encrypter()
-        conf = ProtectConfig(openpgp=encrypter)
-        encmsg = protect(msg, config=conf)
+def test_unobscured_headers():
+    msg = parser.parsestr(EMAIL)
+    encrypter = Encrypter()
+    conf = ProtectConfig(openpgp=encrypter, obscured_headers=[])
+    encmsg = protect(msg, config=conf)
 
-        for header, value in conf.obscured_headers.items():
-            msgheaders = encmsg.get_all(header, [])
-            if msgheaders:
-                self.assertEqual(msgheaders, [value])
+    assert encmsg['from'] == FROM
+    assert encmsg['to'] == TO
+    assert encmsg['subject'] == SUBJECT
 
-        encpart = p.parsestr(encrypter.data)
-        self.assertEqual(encpart.get_content_type(), "multipart/mixed")
-        rfc822part = encpart.get_payload(0)
-        self.assertEqual(rfc822part.get_content_type(), "text/rfc822-headers")
-        rfc822body = "Subject: %s\n" % (SUBJECT,)
-        self.assertEqual(rfc822part.get_payload(), rfc822body)
-        self.assertEqual(encpart.get_payload(1).get_payload(),
-                         BODY+'\n')
 
-    def test_pgp_signed_mime(self):
-        p = Parser()
-        msg = p.parsestr(EMAIL)
-        signer = Signer()
-        conf = ProtectConfig(openpgp=signer)
-        encmsg = protect(msg, encrypt=False, config=conf)
+def test_obscured_headers():
+    msg = parser.parsestr(EMAIL)
+    encrypter = Encrypter()
+    conf = ProtectConfig(openpgp=encrypter)
+    encmsg = protect(msg, config=conf)
 
-        b64body = b64encode(six.b(BODY+'\n'))
-        self.assertEqual(six.b(encmsg.get_payload(0).get_payload()), b64body)
-        self.assertEqual(encmsg.get_payload(1).get_payload(), signer.signature)
-        self._assert_body(signer.data, b64body.decode('utf-8'))
-        self.assertEqual(encmsg.get_content_type(), "multipart/signed")
+    for header, value in conf.obscured_headers.items():
+        msgheaders = encmsg.get_all(header, [])
+        if msgheaders:
+            assert msgheaders == [value]
 
-    def test_signed_headers(self):
-        p = Parser()
-        msg = p.parsestr(EMAIL)
-        signer = Signer()
-        conf = ProtectConfig(openpgp=signer)
-        signmsg = protect(msg, encrypt=False, config=conf)
+    encpart = parser.parsestr(encrypter.data)
+    assert encpart.get_content_type() == "multipart/mixed"
 
-        self.assertEqual(signmsg['from'], FROM)
-        self.assertEqual(signmsg['to'], TO)
-        self.assertEqual(signmsg['subject'], SUBJECT)
+    rfc822part = encpart.get_payload(0)
+    assert rfc822part.get_content_type() == "text/rfc822-headers"
 
-        signedpart = signmsg.get_payload(0)
-        self.assertEqual(signedpart['from'], FROM)
-        self.assertEqual(signedpart['to'], TO)
-        self.assertEqual(signedpart['subject'], SUBJECT)
+    rfc822body = "Subject: %s\n" % (SUBJECT,)
+    assert rfc822part.get_payload() == rfc822body
+    assert encpart.get_payload(1).get_payload() == BODY+'\n'
 
-    def _assert_body(self, data, body):
-        p = Parser()
-        msg = p.parsestr(data)
-        self.assertEqual(msg.get_payload(), body)
+
+def test_pgp_signed_mime():
+    msg = parser.parsestr(EMAIL)
+    signer = Signer()
+    conf = ProtectConfig(openpgp=signer)
+    encmsg = protect(msg, encrypt=False, config=conf)
+
+    b64body = b64encode(six.b(BODY+'\n'))
+    assert six.b(encmsg.get_payload(0).get_payload()) == b64body
+    assert encmsg.get_payload(1).get_payload() == signer.signature
+    assert get_body(signer.data) == b64body.decode('utf-8')
+    assert encmsg.get_content_type() == "multipart/signed"
+
+
+def test_signed_headers():
+    msg = parser.parsestr(EMAIL)
+    signer = Signer()
+    conf = ProtectConfig(openpgp=signer)
+    signmsg = protect(msg, encrypt=False, config=conf)
+
+    assert signmsg['from'] == FROM
+    assert signmsg['to'] == TO
+    assert signmsg['subject'] == SUBJECT
+
+    signedpart = signmsg.get_payload(0)
+    assert signedpart['from'] == FROM
+    assert signedpart['to'] == TO
+    assert signedpart['subject'] == SUBJECT
+
+
+def get_body(data):
+    return parser.parsestr(data).get_payload()
 
 
 @implementer(IOpenPGP)
@@ -121,7 +120,3 @@ class Signer(object):
     def sign(self, data):
         self.data = data
         return self.signature
-
-
-if __name__ == "__main__":
-    unittest.main()
